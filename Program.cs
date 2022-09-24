9e21f79c6beaadfbdb32280b2a4f3d8b217d20d3
poojakrp777 <poojakrp777@gmail.com>
2022-09-24 18:55:15 +0530

    Starting Position is zero with player 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int StartPosition = 0;
+            Console.WriteLine("Starting Position of Player 1 is :" + StartPosition);
+        }
+    }
+}
