548ca7edb803580b45480b87f98249f246c13c0c
poojakrp777 <poojakrp777@gmail.com>
2022-09-30 19:12:19 +0530

    Roll a dice adding UC2

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ namespace SnakeandLadder
         static void Main(string[] args)
         {
             int StartPosition = 0;
+            int ran = 0;
+            Random random = new Random(); //Random function can generate random number
+            ran = random.Next(1, 7);//Considering range from 1 to 6 i.e below 7
+            Console.WriteLine("Rolled Dice");
+            Console.WriteLine("Number got: " + ran);
             Console.WriteLine("Starting Position of Player 1 is :" + StartPosition);
+            Console.ReadLine();
         }
     }
 }
